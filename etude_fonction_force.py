05d0676928d4b06c6b0e593a3a9020fd8988ad70
axelsauvage <axelsauvage10@gmail.com>
2015-10-18 21:55:29 +0200

    Création de la fonction force
    Création de la fonction qui permet de calculer la force appliquée par le
    conducteur en fonction de la situation devant lui.
    Explications :
    g = 1 => accélération maximale
    g = -1 -> freinage maximal
    La distance est relative à la distance de sécurité et la vitesse est
    relative à celle de la voiture de devant

---
+++ etude_fonction_force.py
@@ -0,0 +1,47 @@
+# coding: utf8
+
+from pylab import *
+from mpl_toolkits.mplot3d import Axes3D
+
+# Paramètres
+vitesse_limite = 36
+distance_securite = 2 * vitesse_limite
+vitesse_caracteristique = vitesse_limite
+
+def g(delta_v, delta_h):
+    """
+    Etude de la fonction qui permet de déterminer la force appliquée par le conducteur en fonction des différents paramètres
+    :param delta_v: vitesse relative par rapport à la voiture de devant
+    :param delta_h: distance relative par rapport à la distance de sécurité
+    :return: la force résultante appliquée par le conducteur
+    """
+    c_v = (-np.arctan(delta_h/distance_securite + 1)/np.pi + 0.5) * 40
+    c_h = (-np.arctan(delta_v/vitesse_limite)/np.pi + 0.5) * 10
+    r = (np.arctan(c_v*delta_v/vitesse_limite + c_h*(delta_h/distance_securite + 1))/np.pi + 0.5)*2 - 1
+    return r
+
+def tracer(xmin, xmax, ymin, ymax):
+    fig = figure()
+    ax = fig.gca(projection='3d')
+    X = np.arange(xmin, xmax, 0.4)
+    Y = np.arange(ymin, ymax, 0.4)
+    X, Y = np.meshgrid(X, Y)
+
+    surf = ax.plot_surface(X, Y, g(X, Y), rstride=30, cstride=30, cmap=cm.coolwarm, linewidth=1)
+
+    ax.zaxis.set_major_locator(LinearLocator(10))
+    ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+
+    fig.colorbar(surf, shrink=0.5, aspect=5)
+
+    ax.set_zlim(-1, 1)
+    ax.set_xlim(xmin, xmax)
+    ax.set_ylim(ymin, ymax)
+
+    ax.set_xlabel("vitesse relative (m/s)")
+    ax.set_ylabel("distance relative (m)")
+    ax.set_zlabel("g(distance, vitesse)")
+
+    show()
+
+tracer(-vitesse_limite, vitesse_limite, -distance_securite, 500)
