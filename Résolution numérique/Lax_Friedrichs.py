0d7e2ab63a2006798d5f7f6490840906108b7abe
axelsauvage <axelsauvage10@gmail.com>
2016-01-19 19:24:46 +0100

    Ajout de commentaires

--- Résolution numérique/Lax_Friedrichs.py
+++ Résolution numérique/Lax_Friedrichs.py
@@ -4,41 +4,61 @@ import numpy as np
 import pylab as plt
 from matplotlib import animation
 
+"""
+Résolution numérique de l'équation aux dérivées partielles:
+    du/dx + df(u)/dt = 0
+    u(x,0) = u0(x)
+"""
+
+# Paramètres de la simulation numérique
 xmax = 20
 tmax = 40
+# Pas d'intégration
 Dt = 0.1
 Dx = 0.1
 
+# Nombre de points de calcul
 Nx = int(xmax // Dx) + 1
 Nt = int(tmax // Dt) + 1
 
 def f(u):
+    """ Fonction f(u) de l'équation différentielle """
     return 0.5*u**2
 
 def initialisation():
+    """ Renvoie u0 la condition initiale du problème """
     t = gaussienne()
     return t
 
 def bord(T, t):
+    """ Conditions aux bords pour x=0 et x=xmax au temps d'intégration t """
     T[0, t] = 0
     T[-1, t] = 0
     return T
 
+
+# Création de différentes fonctions u0 initales
 def choc():
+    """ Répartition constante égale à 1 puis nulle """
     t = np.zeros(Nx)
     for x in range(0, Nx//2):
         t[x] = 1
     return t
 
 def gaussienne():
+    """ Répartition gaussienne centrée en x=10 """
     return np.array([np.exp(-(x*Dx - 10)**2) for x in range(0, Nx)])
 
-def afficher(t):
+
+# Fonctions permettant l'affichage des résultats et leur sauvegarde
+def afficher(T):
+    """ Affichage simple de u(x, t) au temps t en fonction de x """
     X = np.arange(0, Nx) * Dx
-    plt.plot(X, t)
+    plt.plot(X, T)
     plt.show()
 
 def afficher_2D(T):
+    """ Affichage d'un graphique 2D permettant de visualiser l'évolution de u(x,t) en fonction du temps """
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
     plt.imshow(T, origin='lower', interpolation='nearest', cmap=plt.cm.afmhot, aspect='auto', extent=[0, tmax, 0, xmax])
@@ -48,6 +68,7 @@ def afficher_2D(T):
     plt.show()
 
 def visualisation(T):
+    """ Animation de u(x,t) en fonction du temps avec sauvegarde sous forme d'un fichier mp4 """
     fig = plt.figure()
     data, = plt.plot([], [])
     plt.xlim(0, xmax)
@@ -71,7 +92,8 @@ def visualisation(T):
 
     # anim.save('Résultats/bouchon_01.mp4', writer=writer)
 
-def iteration():
+
+def resolution():
     T = np.zeros((Nx, Nt))
     u0 = initialisation()
 
@@ -85,4 +107,4 @@ def iteration():
     visualisation(T)
     afficher_2D(T)
 
-iteration()
+resolution()
